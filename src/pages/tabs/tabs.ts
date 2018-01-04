1c6880131d5ad18326a142bb3f46e742e200d1d7
mayadahagithub <mayadahnaji@gmail.com>
2018-01-04 07:03:27 +0300

    add showpage to toolbar

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,9 +4,11 @@ import { AboutPage } from '../about/about';
 import {ContactPage } from '../contact/contact';
 import {  HumancasePage } from '../humancase/humancase';
 import {  AddhumancasesPage } from '../addhumancases/addhumancases';
+import { ShowhumancasePage } from '../showhumancase/showhumancase';
 import { HomePage } from '../home/home';
 import {  NeedingPage }  from  '../needing/needing';
 import {AddneedingPage }  from  '../addneeding/addneeding';
+import  {ShowneedingPage }  from  '../showneeding/showneeding';
 import { MapPage }  from  '../map/map';
 @Component({
   templateUrl: 'tabs.html'
@@ -15,12 +17,18 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root =  HumancasePage;
-  tab3Root =AddhumancasesPage;  
-  tab4Root = MapPage;
-  tab5Root =  NeedingPage;
-  tab6Root =  AddneedingPage;
-  tab7Root = AboutPage;
-  tab8Root = ContactPage;
+  tab3Root =AddhumancasesPage; 
+  
+  tab4Root = ShowhumancasePage ; 
+
+  tab5Root = MapPage;
+  tab6Root =  NeedingPage;
+  tab7Root =  AddneedingPage;
+
+  tab8Root = ShowneedingPage;
+  
+  tab9Root = AboutPage;
+  tab10Root = ContactPage;
   
   constructor() {
 
