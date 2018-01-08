c3749ee7fc1e2bff294d70e9b2be23315c0af7e9
mayadahagithub <mayadahnaji@gmail.com>
2018-01-08 23:50:24 +0300

    modified needing and map and contact and humancases pages

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { ContactPage } from '../contact/contact';
+
 
 
 @IonicPage()
@@ -13,8 +15,15 @@ export class MapPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+
+  goToDetail(){
+    this.navCtrl.push  (ContactPage)
+  }
+
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
   }
+
 
 }
